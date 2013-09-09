54b72d152ba673aa05475b52ec5d1cb2d8657b47
fedeagb@gmail.com <fedeagb@gmail.com@9d0e269b-3c92-383c-628a-ff577347880a>
2013-09-09 16:00:58 +0000

    Complila sin perdia de memoria. Solucionado el tema de los caracteres especiales, ya los procesa sin problemas. Tambien aparentemente funcionaba sin problemas la entrada estandar, lo unico q no tiene con que parar a menos q se interrumpa el programa (con ctrl+C)

--- trunk/tp0.c
+++ trunk/tp0.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define VERSION 2
+#define VERSION 3
 
 // Definicion de funcines
 
@@ -57,25 +57,53 @@ int procesarArchivo(FILE* fd){
 	int nextChar = fgetc(fd);
 	size_t sizeOfChar = sizeof(unsigned char);
 	unsigned char* line = malloc(sizeOfChar);
+	if(!line) {
+		fprintf(stderr, "Problema al querer alocar memoria\n");
+		return -1;
+	}
 	int lineWidth = 0;
 	int result = 0;
+	int aux_special;
 	while(nextChar != EOF) {
+		if(nextChar > 127) { // Para solucionar tema de caracteres especiales
+			lineWidth += 2;
+			line = (unsigned char*) realloc(line, lineWidth*sizeOfChar);
+			if(!line) {
+				fprintf(stderr, "Problema al querer alocar memoria\n");
+				return -1;
+			}
+			aux_special = nextChar;
+			nextChar = fgetc(fd);
+			line[lineWidth - 2] = (unsigned char) nextChar;
+			line[lineWidth - 1] = (unsigned char) aux_special;
+			nextChar = fgetc(fd);
+			continue;
+		}
 		if(nextChar != '\n') {
 			lineWidth++;
 			line = (unsigned char*) realloc(line,lineWidth*sizeOfChar);
+			if(!line) {
+				fprintf(stderr, "Problema al querer alocar memoria\n");
+				return -1;
+			}
 			line[lineWidth - 1] = (unsigned char) nextChar;
-		} else {
+		} 
+		else {
 			unsigned char* inverseLine = (unsigned char*) malloc((lineWidth + 1)*sizeOfChar);
 			for(int pos = 0; pos < lineWidth; pos++){
 				inverseLine[pos] = line[(lineWidth - 1) - pos];
 			}
 			inverseLine[lineWidth] = '\n';
 			int outStatus = fwrite ( inverseLine, sizeOfChar, lineWidth + 1, stdout);
+			if( outStatus != lineWidth + 1) {
+				fprintf(stderr, "Error al escribir en salida\n");
+				result = 2;
+			}
 			free(inverseLine);
 			lineWidth = 0;
 		}
 		nextChar = fgetc(fd);
 	}
+	free(line);
 	return result;
 }
-
